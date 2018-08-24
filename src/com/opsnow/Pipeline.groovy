28ffac3d11cd13a4873a841f427ea1d254191f82
nalbam <me@nalbam.com>
2018-08-24 19:49:44 +0900

    base_domain

--- src/com/opsnow/Pipeline.groovy
+++ src/com/opsnow/Pipeline.groovy
@@ -23,7 +23,6 @@ def scan(name = "", branch = "master", namespace = "devops", base_domain = "") {
     this.jenkins = scan_domain("jenkins", namespace, base_domain)
     if (this.jenkins) {
         this.base_domain = this.jenkins.substring(this.jenkins.indexOf('.') + 1)
-        echo "# base_domain: $base_domain"
     }
 
     this.chartmuseum = scan_domain("chartmuseum", namespace, base_domain)

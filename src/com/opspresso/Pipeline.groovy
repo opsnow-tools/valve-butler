223c159db23a7a21c8a56d26acdd8a4c929aae53
nalbam <me@nalbam.com>
2018-09-03 19:35:36 +0900

    token

--- src/com/opspresso/Pipeline.groovy
+++ src/com/opspresso/Pipeline.groovy
@@ -310,11 +310,13 @@ def draft_up(name = "", namespace = "", cluster = "", base_domain = "") {
 }
 
 def slack(token = "", color = "", title = "", message = "", footer = "") {
-    // try {
-        sh """
-            curl -sL toast.sh/helper/slack.sh | bash -s -- --token='$token' \
-            --color='$color' --title='$title' --footer='$footer' '$message'
-        """
-    // } catch (ignored) {
-    // }
+    if (token) {
+        // try {
+            sh """
+                curl -sL toast.sh/helper/slack.sh | bash -s -- --token='$token' \
+                --color='$color' --title='$title' --footer='$footer' '$message'
+            """
+        // } catch (ignored) {
+        // }
+    }
 }

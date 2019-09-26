2e8d48d3383b73f1442a12b609d7f12d22270669
longlg88 <longlg88@naver.com>
2019-09-26 03:39:28 +0000

    test butler

--- src/com/opsnow/valve/v7/Butler.groovy
+++ src/com/opsnow/valve/v7/Butler.groovy
@@ -521,6 +521,7 @@ def mvn_deploy(source_root = "") {
 }
 
 def mvn_sonar(source_root = "", sonarqube = "", sonar_token="") {
+    echo "${sonar_token}"
     if (!sonarqube) {
         if (!this.sonarqube) {
             echo "mvn_sonar:sonarqube is null."

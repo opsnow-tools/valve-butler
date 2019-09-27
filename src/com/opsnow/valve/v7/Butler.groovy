26c815b56950181c019ebc70959a49a319fd207f
longlg88 <longlg88@naver.com>
2019-09-27 06:33:11 +0000

    modify

--- src/com/opsnow/valve/v7/Butler.groovy
+++ src/com/opsnow/valve/v7/Butler.groovy
@@ -679,7 +679,7 @@ def mvn_deploy(source_root = "") {
     }
 }
 
-def mvn_sonar(sonar_token = "", source_root = "", sonarqube = "") {
+def mvn_sonar(source_root = "", sonarqube = "") {
     //if (!sonar_token) {
     //  echo "sonar token is null. Check secret text 'sonar-token' in credentials"
     //  throw new RuntimeException("sonar token is null.")
@@ -691,14 +691,16 @@ def mvn_sonar(sonar_token = "", source_root = "", sonarqube = "") {
         }
         sonarqube = "https://${this.sonarqube}"
     }
-    source_root = get_source_root(source_root)
-    dir("${source_root}") {
-        settings = get_m2_settings()
-        if (!sonar_token) {
-          sh "mvn sonar:sonar ${settings} -Dsonar.host.url=${sonarqube} -DskipTests=true"
-        } else {
-          sh "mvn sonar:sonar ${settings} -Dsonar.login=${sonar_token} -Dsonar.host.url=${sonarqube} -DskipTests=true"
-        }
+    withCredentials([string(credentialsId: 'sonar-token', variable: 'sonar_token')]){
+      source_root = get_source_root(source_root)
+      dir("${source_root}") {
+          settings = get_m2_settings()
+          if (!sonar_token) {
+            sh "mvn sonar:sonar ${settings} -Dsonar.host.url=${sonarqube} -DskipTests=true"
+          } else {
+            sh "mvn sonar:sonar ${settings} -Dsonar.login=${sonar_token} -Dsonar.host.url=${sonarqube} -DskipTests=true"
+          }
+      }
     }
 }
 

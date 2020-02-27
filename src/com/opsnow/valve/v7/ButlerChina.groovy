4af2f437b4c62bca2e825815c819fd1a2accf66a
gelius <gelius7@gmail.com>
2020-02-27 20:06:46 +0900

    fix

--- src/com/opsnow/valve/v7/ButlerChina.groovy
+++ src/com/opsnow/valve/v7/ButlerChina.groovy
@@ -187,7 +187,7 @@ def env_cluster(cluster = "") {
     load_variables()
 
     // ecr repository uri
-    set_image_repository()
+    set_image_repository("${registry}")
 }
 
 def env_namespace(namespace = "") {
